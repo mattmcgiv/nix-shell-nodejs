f7bb1dd39650240f72a358978434746c194c5632
Matt McGivney <matt@mattmcgivney.com>
2023-03-27 14:23:43 -0600

    Update shell.nix
    Update nix packages tarball url

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 # use command nix-shell to enter an environment with the following dependencies
 
 # locks to a specific version of nixpkgs for reproducible environment
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz") {} }:
 
 # search for packages: https://search.nixos.org/packages
 pkgs.mkShell {
