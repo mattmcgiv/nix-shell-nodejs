70202c27fcc5800ba0fdac879da681d9b5c56f54
Matt McGivney <matt@mattmcgivney.com>
2024-01-12 08:37:22 -0700

    Bump node version 18->20

--- shell.nix
+++ shell.nix
@@ -1,13 +1,13 @@
 # use command nix-shell to enter an environment with the following dependencies
 
 # locks to a specific version of nixpkgs for reproducible environment
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz") {} }:
 
 # search for packages: https://search.nixos.org/packages
 pkgs.mkShell {
   buildInputs = [
     # node version (comes with npm)
-    pkgs.nodejs-18_x
+    pkgs.nodejs_20
     # *globally* installed node package
     # pkgs.nodePackages.aws-cdk
     # *keep in mind* local npm install works if you don't need the package globally
